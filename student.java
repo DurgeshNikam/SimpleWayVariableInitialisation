78ee039013706dda2bff08bd3785a2dc32906182
DurgeshNikam <nikam.durgesh1@gmail.com>
2023-02-14 13:34:03 +0530

    SimpleWayVaribleInitialisation

---
+++ student.java
@@ -0,0 +1,16 @@
+public class student
+{
+
+	int rollno=101;
+	String name="ram";
+{
+	int y=300;
+	System.out.println(y);
+}
+public static void main(String[]args)
+{
+	student s=new student();
+System.out.println(s.rollno);
+System.out.println(s.name);
+}
+}
